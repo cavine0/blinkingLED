b0b62b9ff0e268554d14026be784fdf541b7396a
cavine0 <odhiambocavine68@gmail.com>
2024-11-15 10:30:01 +0300

    proximity.group1 code

---
+++ proximity.group1.py
@@ -0,0 +1,4 @@
+sound_speed = 330
+time_taken = float(input("How long did it take to hear the echo ? :"))
+distance = (sound_speed * time_taken) / 2
+print(f'The distance to the object is{distance} meters')
